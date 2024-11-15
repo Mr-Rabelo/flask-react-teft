bc0f38c2b65c99d7fb5c1bf264399dbb8197d9d9
Arthur Rabelo <66283633+Mr-Rabelo@users.noreply.github.com>
2024-11-14 22:05:43 -0300

    Repository Creation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          meu nome = {window.meuNome}
         </p>
         <a
           className="App-link"
